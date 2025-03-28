6974d36defac9aceb63cd40133f0a934143f5027
Binard2112 <mait58674@gmail.com>
2025-03-28 11:11:09 +0700

    redux and redux toolkit

---
+++ ReduxAndReduxToolkit/old-local-blog/src/App.tsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1 className='font-bold text-4xl'>hello world</h1>
+      <button className='border bg-amber-500'>add</button>
+    </div>
+  )
+}
+
+export default App
